58a7ffc6739c13b25e609bc843a1db80686cc78e
Mensen <research.mensen@gmail.com>
2014-12-16 11:19:01 -0600

    variable name changes

--- Dependencies/swa_getFiles.m
+++ Dependencies/swa_getFiles.m
@@ -1,26 +1,32 @@
-function fileList = swa_getFiles(dirName, pattern)
+function fileList = swa_getFiles(dir_name, pattern)
+% function to recursively find files matching a certain pattern
 
-  dirData = dir(dirName);      %# Get the data for the current directory
-  dirIndex = [dirData.isdir];  %# Find the index for directories
-  fileList = {dirData(~dirIndex).name}';  %'# Get a list of the files
-%   if ~isempty(fileList)
-%     fileList = cellfun(@(x) fullfile(dirName,x),...  %# Prepend path to files
-%                        fileList,'UniformOutput',false);
-%   end
-  
-  if ~isempty(fileList)
-      fileList = cellfun(@(x) fullfile(dirName,x),...  %# Prepend path to files
-          fileList,'UniformOutput',false); 	
-      matchstart = regexp(fileList, pattern); 	
-      fileList = fileList(~cellfun(@isempty, matchstart)); 
-  end
-  
-  subDirs = {dirData(dirIndex).name};  %# Get a list of the subdirectories
-  validIndex = ~ismember(subDirs,{'.','..'});  %# Find index of subdirectories
-                                               %#   that are not '.' or '..'
-  for iDir = find(validIndex)                  %# Loop over valid subdirectories
-    nextDir = fullfile(dirName,subDirs{iDir});    %# Get the subdirectory path
-    fileList = [fileList; swa_getFiles(nextDir, pattern)];  %# Recursively call getAllFiles
-  end
+% Get the data for the current directory
+dirData = dir(dir_name);
+% Find the index for directories
+dirIndex = [dirData.isdir];
+% Get a list of the files
+fileList = {dirData(~dirIndex).name}';
+
+if ~isempty(fileList)
+    % Prepend path to files
+    fileList = cellfun(@(x) fullfile(dir_name,x),...
+        fileList,'UniformOutput',false);
+    matchstart = regexp(fileList, pattern);
+    fileList = fileList(~cellfun(@isempty, matchstart));
+end
+
+% Get a list of the subdirectories
+subDirs = {dirData(dirIndex).name};
+% Find index of subdirectories that are not '.' or '..'
+validIndex = ~ismember(subDirs,{'.','..'});
+
+% Loop over valid subdirectories
+for iDir = find(validIndex)
+    % Get the subdirectory path
+    nextDir = fullfile(dir_name,subDirs{iDir});
+    % Recursively call getAllFiles
+    fileList = [fileList; swa_getFiles(nextDir, pattern)];
+end
 
 end
