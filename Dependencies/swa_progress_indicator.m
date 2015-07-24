69fe026ac8aa73c943a18476ffc768e0f734de5c
Mensen <research.mensen@gmail.com>
2015-07-24 16:50:52 -0500

    initial commit for the command line progress indicator function

---
+++ Dependencies/swa_progress_indicator.m
@@ -0,0 +1,43 @@
+function swa_progress_indicator(type, current, maximum)
+% command line progress bar that erases past progress
+
+% usage:
+% swa_progress_indicator('initialise', 'percentage complete')
+% 
+% for n = 1:30
+%    
+%     WaitSecs(0.1);
+%     
+%     swa_progress_indicator('update', n, 30);
+%     
+% end
+
+
+switch type
+    
+    case 'initialise'
+        
+        fprintf(1, ['\n', current, ': 0.00 %%']);
+        
+    case 'update'
+   
+        percentage = current / maximum * 100;
+        
+        if percentage <= 10
+            
+            fprintf(1, '\b\b\b\b\b\b\b %.2f %%', percentage);
+
+            
+        else
+            
+            fprintf(1, '\b\b\b\b\b\b\b\b %.2f %%', percentage);
+            
+        end
+   
+        if current == maximum
+            
+            fprintf(1, '\n');
+            
+        end
+        
+end
