b1dc22701662981a2cc996662473a1ed2684bd16
Mensen <research.mensen@gmail.com>
2015-07-24 16:52:13 -0500

    can now load data using single input specifying file and path together

--- Dependencies/swa_load_previous.m
+++ Dependencies/swa_load_previous.m
@@ -1,9 +1,16 @@
 function [Data, Info, SW] = swa_load_previous(fileName, filePath)
 
+% check arguments
 if nargin < 1;
     [fileName, filePath] = uigetfile('*.mat');
+elseif nargin < 2
+    % attempt to get fileparts
+    [filePath, fileName, ext] = fileparts(fileName);
+    fileName = [fileName, ext];
 end
-    load(fullfile(filePath, fileName), '-mat');
+
+% load the file (only data pointer)
+load(fullfile(filePath, fileName), '-mat');
 
 
 % check for correct file
